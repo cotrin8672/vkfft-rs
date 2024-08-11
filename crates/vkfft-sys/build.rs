4042cdb12412f99bc2b10ca9b7f23787d29062cb
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2024-08-11 08:57:39 +0200

    Fix vkfft-sys crate to build with newest vkfft, bindgen

--- crates/vkfft-sys/build.rs
+++ crates/vkfft-sys/build.rs
@@ -1,18 +1,25 @@
 extern crate bindgen;
 extern crate cc;
 
+use bindgen::Bindings;
+use regex::Regex;
+use std::collections::HashMap;
 use std::error::Error;
 use std::path::{Path, PathBuf};
 
-use bindgen::Bindings;
-
-fn build_lib<O, LD, L, const N: usize, const M: usize>(out_dir: O, library_dirs: LD, libraries: L, defines: &[(&str, &str); N], include_dirs: &[String; M]) -> Result<(), Box<dyn Error>>
+fn build_lib<O, LD, L, const N: usize, const M: usize>(
+  out_dir: O,
+  library_dirs: LD,
+  libraries: L,
+  defines: &[(&str, &str); N],
+  include_dirs: &[String; M],
+) -> Result<(), Box<dyn Error>>
 where
   O: AsRef<Path>,
   LD: Iterator,
   LD::Item: AsRef<str>,
   L: Iterator,
-  L::Item: AsRef<str>
+  L::Item: AsRef<str>,
 {
   let mut build = cc::Build::default();
 
@@ -31,53 +38,48 @@ where
     build.flag(format!("-l{}", library.as_ref()).as_str());
   }
 
-  build
-    .cargo_metadata(true)
-    .static_flag(true);
-  
+  build.cargo_metadata(true).static_flag(true);
+
   for (key, value) in defines.iter() {
     build.define(*key, Some(*value));
   }
-  
+
   for include_dir in include_dirs.iter() {
     build.include(include_dir);
   }
 
-  
   build.compile("vkfft");
 
   Ok(())
 }
 
-fn gen_wrapper<F, const N: usize, const M: usize>(file: F,  defines: &[(&str, &str); N], include_dirs: &[String; M]) -> Result<Bindings, Box<dyn Error>>
+fn gen_wrapper<F, const N: usize, const M: usize>(
+  file: F,
+  defines: &[(&str, &str); N],
+  include_dirs: &[String; M],
+) -> Result<Bindings, Box<dyn Error>>
 where
   F: AsRef<Path>,
 {
-  let base_args = [
-    "-std=c++11".to_string(),
-  ];
-  
-  let defines: Vec<String> = defines.iter().map(|(k, v)| {
-    format!("-D{}={}", k, v)
-  }).collect();
+  let base_args = ["-std=c++11".to_string()];
 
-  let include_dirs: Vec<String> = include_dirs.iter()
-    .map(|s| format!("-I{}", s))
+  let defines: Vec<String> = defines
+    .iter()
+    .map(|(k, v)| format!("-D{}={}", k, v))
     .collect();
 
-  let clang_args = base_args 
+  let include_dirs: Vec<String> = include_dirs.iter().map(|s| format!("-I{}", s)).collect();
+
+  let clang_args = base_args
     .iter()
     .chain(defines.iter())
     .chain(include_dirs.iter());
 
   println!("{:?}", clang_args);
 
-  
-
-
   let res = bindgen::Builder::default()
     .clang_args(clang_args)
-    .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+    .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
     .header(file.as_ref().to_str().unwrap())
     .allowlist_recursively(true)
     .allowlist_type("VkFFTConfiguration")
@@ -94,9 +96,8 @@ where
     .allowlist_function("initializeVkFFT")
     .allowlist_function("deleteVkFFT")
     .allowlist_function("VkFFTGetVersion")
-    
     .generate();
-  
+
   let bindings = match res {
     Ok(x) => x,
     Err(_) => {
@@ -108,6 +109,44 @@ where
   Ok(bindings)
 }
 
+fn process_includes(
+  file: &str,
+  root_path: &str,
+  path_hint: &str,
+  max_recursion: u32
+) -> Result<String, Box<dyn Error>> {
+  let mut included_files: HashMap<String, u32> = HashMap::new();
+  let mut stack: Vec<String> = Vec::new();
+  let mut result: String = std::fs::read_to_string(&format!("{}/{}", root_path, file))?;
+  let re = Regex::new(&format!("#include \"{}(.*)\"", path_hint)).unwrap();
+
+  // Push the initial file onto the stack
+  stack.push(file.into());
+
+  while let Some(capture) = re.captures(&result.clone()) {
+    let current_file = &capture[1];
+    if included_files.contains_key(current_file) {
+      if included_files[current_file] < max_recursion {
+        *included_files.entry(current_file.to_string()).or_insert(1) += 1;
+        result = result.replace(&format!("#include \"{}{}\"",path_hint, current_file),"");
+      } else{
+        result = result.replace(&format!("#include \"{}{}\"",path_hint, current_file),"");
+        continue;
+      }
+    } else {
+      included_files.insert(current_file.to_string(),1);
+    }
+    
+    let current_path = format!("{}/{}", root_path, current_file);
+    let file_content = std::fs::read_to_string(&current_path)?;
+    println!("working on: {}", current_file);
+    println!("#include \"{}{}\"\n",path_hint, current_file);
+    result = result.replace(&format!("#include \"{}{}\"",path_hint, current_file),&file_content);
+  }
+
+  Ok(result)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
   let vkfft_root = std::env::var("VKFFT_ROOT")?;
   let out_dir = std::env::var("OUT_DIR")?;
@@ -116,7 +155,7 @@ fn main() -> Result<(), Box<dyn Error>> {
   let library_dirs = [
     format!("{}/build/glslang-main/glslang", vkfft_root),
     format!("{}/build/glslang-main/glslang/OSDependent/Unix", vkfft_root),
-    format!("{}/build/glslang-main/glslang/OGLCompilersDLL", vkfft_root),
+    format!("{}/build/glslang-main/OGLCompilersDLL", vkfft_root),
     format!("{}/build/glslang-main/SPIRV", vkfft_root),
   ];
 
@@ -127,7 +166,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     "GenericCodeGen",
     "OGLCompiler",
     "vulkan",
-    "SPIRV"
+    "SPIRV",
   ];
 
   for library_dir in library_dirs.iter() {
@@ -138,30 +177,37 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rustc-link-lib={}", library);
   }
 
-
   println!("cargo:rerun-if-changed=wrapper.cpp");
   println!("cargo:rerun-if-changed=build.rs");
 
   let include_dirs = [
     format!("{}/vkFFT", &vkfft_root),
-    format!("{}/glslang-main/glslang/Include", vkfft_root)
+    format!("{}/glslang-main/glslang/Include", vkfft_root),
   ];
 
-  let defines = [
-    ("VKFFT_BACKEND", "0"),
-    ("VK_API_VERSION", "11")
-  ];
+  let defines = [("VKFFT_BACKEND", "0"), ("VK_API_VERSION", "11")];
 
-  let wrapper = std::fs::read_to_string(format!("{}/vkFFT/vkFFT.h", vkfft_root))?
-    .replace("static inline", "");
+  let wrapper = process_includes(
+    &format!("../vkFFT.h"),
+    &(vkfft_root + "/vkFFT/vkFFT"),
+    "vkFFT",
+    100
+  )?
+  .replace("static inline ", "");
 
   let rw = out_dir.join("vkfft_rw.hpp");
   std::fs::write(&rw, wrapper.as_str())?;
 
-  build_lib(&out_dir, library_dirs.iter(), libraries.iter(), &defines, &include_dirs)?;
+  build_lib(
+    &out_dir,
+    library_dirs.iter(),
+    libraries.iter(),
+    &defines,
+    &include_dirs,
+  )?;
 
   let bindings = gen_wrapper(&rw, &defines, &include_dirs)?;
   bindings.write_to_file(out_dir.join("bindings.rs"))?;
-  
+
   Ok(())
 }
